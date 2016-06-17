52ffcca0a84f86b4e06c05ef6e7afc11e95f1510
Keith Lim <crowball@gmail.com>
2016-06-17 12:12:51 +0800

    added total purchase
    Signed-off-by: Keith Lim <crowball@gmail.com>

---
+++ src/keith/lim/Company.java
@@ -0,0 +1,7 @@
+package keith.lim;
+
+/**
+ * Created by kiiiiibble on 6/17/2016.
+ */
+public class Company {
+}
--- src/keith/lim/TotalPurchase.java
+++ src/keith/lim/TotalPurchase.java
@@ -3,12 +3,19 @@ package keith.lim;
 
 //Try to make a total PO receipt.
 public class TotalPurchase {
-    Item[] basket = new Item[] {new Item(1, "Chihuahua", "Dog", 5000, 5, "Crates")
-        , new Item(2, "Shark", "Fish", 10000, 3, "Aquariums")
-            , new Item(3, "Gerbil", "Expensive Rodent", 50000, 10, "Packs")};
 
-    for(int z=0; z<basket.length ; z++){
-        int totprice[n] = Item[n].getQty * Item[n].getPrice;
-        System.out.println(totprice[n]);
+    public static void main(String[] args) {
+
+        Item basket[] = new Item[]{new Item(1, "Chihuahua", "Dog", 5000, 5, "Crates")
+                , new Item(2, "Shark", "Fish", 10000, 3, "Aquariums")
+                , new Item(3, "Gerbil", "Expensive Rodent", 50000, 10, "Packs")};
+
+        for (int z = 0; z < 3; z++) {
+
+            float totprice = 0;
+            totprice = basket[z].getQty() * basket[z].getPrice();
+            System.out.println(totprice);
+        }
     }
+
 }
